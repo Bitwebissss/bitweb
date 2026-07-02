e62ff1d5e9252297010696abc01688a67c6956d0
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2026-07-02 15:15:03 +0300

    Update blockstorage.cpp
    Tree-SHA512: b2dbd21d4cc9e4b273c5e8e97c4dd76c843c38b1c05e26681973aa46bc1d72684eb0408e42db4c243a53ff13eb4f621d07aeae145ed3295f0c9fd816dc12bb0d

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -16,7 +16,6 @@
 #include <kernel/messagestartchars.h>
 #include <kernel/notifications_interface.h>
 #include <kernel/types.h>
-#include <pow.h>
 #include <pow_cache.h>
 #include <primitives/block.h>
 #include <primitives/transaction.h>
